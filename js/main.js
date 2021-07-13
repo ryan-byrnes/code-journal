97136400f7642ff3e8a1e2494bd66664a0fc4cf6
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-07-12 17:07:36 -0700

    Created submit event handler (including all sub-tasks)

--- js/main.js
+++ js/main.js
@@ -9,3 +9,31 @@ imageInput.addEventListener('input', imagePreview);
 function imagePreview(event) {
   image.src = event.target.value;
 }
+
+var formInput = document.querySelector('.input-form');
+
+formInput.addEventListener('submit', submitUserInput);
+
+var entryId = 0;
+var dataModel = [];
+
+function submitUserInput(event) {
+  event.preventDefault();
+  var inputForm = document.querySelector('.input-form');
+  var input = inputForm.elements;
+  var dataObject = {};
+  for (var i = 0; i < input.length; i++) {
+    if (input[i].name === 'title') {
+      dataObject.title = input[i].value;
+    } else if (input[i].name === 'photo-url') {
+      dataObject.url = input[i].value;
+    } else if (input[i].name === 'notes') {
+      dataObject.notes = input[i].value;
+    }
+  }
+  entryId += 1;
+  dataObject.nextEntryId = entryId;
+  dataModel.unshift(dataObject);
+  inputForm.reset();
+  image.src = 'images/placeholder-image-square.jpg';
+}
