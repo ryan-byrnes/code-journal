514f7d44054fd8a67b2f5534f804dce7f2c1f55e
Ryan Byrnes <ryn_byrnes@yahoo.com>
2021-07-13 13:10:47 -0700

    Removed for loop from event handler and used named properties of elements property on the form element to set new object property values.

--- js/main.js
+++ js/main.js
@@ -23,15 +23,9 @@ function submitUserInput(event) {
   var image = document.querySelector('img');
   var inputForm = document.querySelector('.input-form');
   var input = inputForm.elements;
-  for (var i = 0; i < input.length; i++) {
-    if (input[i].name === 'title') {
-      dataObject.title = input[i].value;
-    } else if (input[i].name === 'photo-url') {
-      dataObject.url = input[i].value;
-    } else if (input[i].name === 'notes') {
-      dataObject.notes = input[i].value;
-    }
-  }
+  dataObject.title = input.title.value;
+  dataObject.url = input['photo-url'].value;
+  dataObject.notes = input.notes.value;
   dataObject.entryId = data.nextEntryId;
   data.nextEntryId += 1;
   data.entries.unshift(dataObject);
